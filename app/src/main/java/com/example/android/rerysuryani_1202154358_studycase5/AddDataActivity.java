22208e896ae0650ca12ce24387b022f477e7cb6f
rerysuryani <shin.rery@gmail.com>
2018-03-25 21:56:32 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/android/rerysuryani_1202154358_studycase5/AddDataActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.rerysuryani_1202154358_studycase5;
+
+public class AddDataActivity {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddDataActivity(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
